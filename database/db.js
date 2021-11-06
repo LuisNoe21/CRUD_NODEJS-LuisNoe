1742183fc725b265f7578e647882d13507b011b7
Luis Noe <71455318+LuisNoe21@users.noreply.github.com>
2021-11-06 01:27:03 -0600

    update db.js
    update db.js

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ const conexion = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '',
-    database : 'crud_nodejs'  
+    database : 'luis_rodriguez_db'  
 });
 
 conexion.connect((error)=>{
